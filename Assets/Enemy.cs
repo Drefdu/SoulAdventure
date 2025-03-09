bc69e3e9a8e8816a7465eded1163c68b80610c87
Eduardo <jesuseduardoarciniegamoreno@gmail.com>
2025-03-09 16:10:12 -0600

    enemies avances

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
+{   
+    private bool puedeAtacar = true;
+    private SpriteRenderer spriteRenderer;
+    public float cooldownAtaque;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,8 +21,24 @@ public class Enemy : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player")){
+            puedeAtacar = false;
             GameManager.Instance.perderVida();
+
+            Color color  = spriteRenderer.color;
+            color.a = 0.5f;
+            spriteRenderer.color = color;
+
             other.gameObject.GetComponent<CharactertControler>().SetHit();
         }
+
+        Invoke("ReactivarAtaque", cooldownAtaque);
+    }
+
+    void ReactivarAtaque()
+    {
+        puedeAtacar = true;
+        Color color = spriteRenderer.color;
+        color.a = 1f;
+        spriteRenderer.color = color;
     }
 }
--- Assets/HUD.cs
+++ Assets/HUD.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System;
 public class HUD : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,11 +22,20 @@ public class HUD : MonoBehaviour
 
     public void desactivarVida(int indice)
     {
+        Debug.Log(indice.ToString());
         vidas[indice].SetActive(false);
     }
 
     public void activarVida(int indice)
     {
         vidas[indice].SetActive(true);
+    }
+
+    public void reiniciarVidas()
+    {
+        foreach (var item in vidas)
+        {
+            item.SetActive(true);
+        }
     }
 }
